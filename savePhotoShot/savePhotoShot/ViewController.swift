13d6028a6e61c3ea47d0359ac89d96a59c44187d
hiroakissh <hiroaki.saito1014@gmail.com>
2022-09-24 00:06:44 +0900

    inital project

---
+++ savePhotoShot/savePhotoShot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  savePhotoShot
+//
+//  Created by HiroakiSaito on 2022/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
